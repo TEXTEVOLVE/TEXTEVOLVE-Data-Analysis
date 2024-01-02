4c8d55f6b0413656211f668d21785cf2a058f8f4
Estara Arrant <estarajames@gmail.com>
2024-01-02 15:46:26 +0000

    Add basic comments to merge file code

--- File-Manipulation/ file_merge.py
+++ File-Manipulation/ file_merge.py
@@ -1,21 +1,22 @@
+# import python modules needed to execute task
 from pathlib import Path
 
 # Variables you might want to modify - you can turn these into command-line arguments later
 data_directory = Path("Manuscripts")  # Current working directory is assumed
-output_file_suffix = "_whole.txt"
-excluded_filenames = [".DS_Store", "Other_Thing"]
+output_file_suffix = "_whole.txt"  # this will append the text in quotes to the name of the file created by this code.
+excluded_filenames = [".DS_Store", "Other_Thing"]  # we don't want the code to pay attention to these files
 verses_sort_key = ('Ex14.30', 'Ex14.31', 'Ex15.1', 'Ex15.2', 'Ex15.3', 'Ex15.4', 'Ex15.5', 'Ex15.6',
                    'Ex15.7', 'Ex15.8', 'Ex15.9', 'Ex15.10', 'Ex15.11', 'Ex15.12', 'Ex15.13', 'Ex15.14',
                    'Ex15.16', 'Ex15.17')
 
 
-# Get list of file Paths from specified directory Path
+# Get list of file Paths from specified directory Path - so computer can identify them
 def get_file_list(dir_path):
     return [filepath for filepath in dir_path.iterdir() if
             filepath.is_file() and filepath.name not in excluded_filenames]
 
 
-# Sort file Paths by sort key
+# Sort file Paths by sort key - because python has its own ordering system which does not fit human readability.
 def sort_file_list(filepath_list, sort_key):
     def sort_by_key(file_name, key):
         return key.index(file_name)
@@ -59,6 +60,6 @@ def run():
         # For each directory, merge files
         merge_files_in_directory(d)
 
-
+# this statement tells the python interpreter to run the code.
 if __name__ == '__main__':
     run()
